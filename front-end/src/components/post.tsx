9a94bc563ad105d60b563bf28a036834ff7a31f7
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-06 22:50:45 +0900

    画像の縦横比固定

--- front-end/src/components/post.tsx
+++ front-end/src/components/post.tsx
@@ -1,6 +1,5 @@
 import { Form, Button, Container, Header } from "semantic-ui-react";
 import TagInput from "./tagInput";
-import { WorkType } from "../types/api";
 import { db, storage, fullpath2url } from "../firebase";
 import { SyntheticEvent } from "react";
 import { v4 } from "uuid";
@@ -8,22 +7,70 @@ import hash from "object-hash";
 import { useAuthContext } from "../context/AuthContext";
 import { useHistory } from "react-router";
 
+const sleep = (msec: number) =>
+  new Promise((resolve) => setTimeout(resolve, msec));
+
+const imageTrimming = async (f: File) => {
+  let ans: Blob | null = null;
+  let ok = false;
+  const canvas = document.createElement("canvas");
+  canvas.width = 640;
+  canvas.height = 360;
+  const ctx = canvas.getContext("2d");
+  const reader = new FileReader();
+  const img: any = document.createElement("Img");
+  reader.readAsDataURL(f);
+  reader.onload = () => {
+    img.src = reader.result;
+    img.onload = () => {
+      console.log(img);
+      console.log(img.width, img.height);
+      let sx, sy, sw, sh;
+      const x = img.width;
+      const y = img.height;
+      if (x / 16 > y / 9) {
+        //　横長
+        sy = 0;
+        sw = (16 / 9) * y;
+        sx = (x - sw) / 2;
+        sh = y;
+      } else {
+        sh = (9 / 16) * x;
+        sx = 0;
+        sw = x;
+        sy = (y - sh) / 2;
+      }
+      console.log(sx, sy, sw, sh);
+      ctx.drawImage(img, sx, sy, sw, sh, 0, 0, 640, 360);
+      canvas.toBlob((blob) => {
+        ans = blob;
+        ok = true;
+        console.log(ans);
+      });
+    };
+  };
+  while (!ok) {
+    await sleep(10);
+  }
+  return ans;
+};
+
 const Post = () => {
-  const { user } = useAuthContext();
+  const { user }: any = useAuthContext();
   const history = useHistory();
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async(e: any) => {
     //画像を先に上げる
     const storageRef = storage.ref();
     const file = e.target.thumb.files[0];
-    console.log(user);
     if (file == null) {
       alert("file not selected");
       return;
     }
+    const blobThumb = await imageTrimming(file);
     const filename = v4().toString();
     storageRef
       .child(`images/thumbnail/${filename}`)
-      .put(file)
+      .put(blobThumb)
       .then((e: any) => {})
       .catch((err) => alert(err.message));
 
