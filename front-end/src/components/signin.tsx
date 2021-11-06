065765b3c2ec7f970e225d9accdf2a41cc165d78
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-06 14:52:20 +0900

    サインイン、サインアウトのエラーなど

--- front-end/src/components/signin.tsx
+++ front-end/src/components/signin.tsx
@@ -1,27 +1,55 @@
-import { ReactEventHandler } from "react";
+import { Message, Container, Input, Button } from "semantic-ui-react";
+import { useState } from "react";
 import { auth } from "../firebase";
 
 const SignIn = () => {
+  const [err, setErr] = useState("");
   const handleSubmit = (e: any) => {
     e?.preventDefault();
     const { email, password } = e.target.elements;
     console.log(email.value, password.value);
-    auth.signInWithEmailAndPassword(email.value, password.value);
+    auth
+      .signInWithEmailAndPassword(email.value, password.value)
+      .catch((err: Error) => {
+        setErr(err.message);
+      })
+      .then(() => {
+        window.location.href = "/";
+      });
   };
   return (
-    <div>
-      <h1>ログイン</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>email</label>
-          <input name="email" type="email"></input>
-        </div>
-        <div>
-          <label>パスワード</label>
-          <input name="password" type="password"></input>
-        </div>
-        <button>ログイン</button>
-      </form>
+    <div style={{ marginTop: "65px" }}>
+      <Container textAlign="center">
+        <h1>サインイン</h1>
+        {err != "" ? <Message warning>{err}</Message> : ""}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label
+              style={{
+                width: "100px",
+                display: "inline-block",
+                marginBottom: "20px",
+              }}
+            >
+              e-mail
+            </label>
+            <Input name="email" type="email"></Input>
+          </div>
+          <div>
+            <label
+              style={{
+                width: "100px",
+                display: "inline-block",
+                marginBottom: "20px",
+              }}
+            >
+              パスワード
+            </label>
+            <Input name="password" type="password"></Input>
+          </div>
+          <Button primary>サインイン</Button>
+        </form>
+      </Container>
     </div>
   );
 };
--- front-end/src/components/signup.tsx
+++ front-end/src/components/signup.tsx
@@ -1,27 +1,61 @@
-import { ReactEventHandler } from "react";
+import { useState } from "react";
 import { auth } from "../firebase";
+import { Message, Container, Input, Button } from "semantic-ui-react";
 
 const SignUp = () => {
+  const [err, setErr] = useState("");
+  const [completion, setCompletion] = useState("");
   const handleSubmit = (e: any) => {
     e?.preventDefault();
     const { email, password } = e.target.elements;
     console.log(email.value, password.value);
-    auth.createUserWithEmailAndPassword(email.value, password.value);
+    auth
+      .createUserWithEmailAndPassword(email.value, password.value)
+      .then((user) => {
+        setCompletion("complete");
+      })
+      .catch((err: Error) => {
+        setErr(err.message);
+      });
   };
   return (
-    <div>
-      <h1>ユーザ登録</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>email</label>
-          <input name="email" type="email"></input>
-        </div>
-        <div>
-          <label>パスワード</label>
-          <input name="password" type="password"></input>
-        </div>
-        <button>登録</button>
-      </form>
+    <div style={{ marginTop: "65px" }}>
+      <Container textAlign="center">
+        <h1>ユーザ登録</h1>
+
+        {err != "" ? <Message warning>{err}</Message> : ""}
+        {completion == "" ? (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label
+                style={{
+                  width: "100px",
+                  display: "inline-block",
+                  marginBottom: "20px",
+                }}
+              >
+                e-mail
+              </label>
+              <Input name="email" type="email"></Input>
+            </div>
+            <div>
+              <label
+                style={{
+                  width: "100px",
+                  display: "inline-block",
+                  marginBottom: "20px",
+                }}
+              >
+                パスワード
+              </label>
+              <Input name="password" type="password"></Input>
+            </div>
+            <Button primary>登録</Button>
+          </form>
+        ) : (
+          <p>登録が完了しました</p>
+        )}
+      </Container>
     </div>
   );
 };
