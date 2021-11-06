5e17e55162c917d8192a60e94b1f897cc024aaa7
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-06 13:37:04 +0900

    ページ整理

--- front-end/src/components/work.tsx
+++ front-end/src/components/work.tsx
@@ -6,6 +6,7 @@ function Work() {
   const tags = ["aiueo", "ttt"];
   return (
     <section>
+      <h2>作品カードサンプル</h2>
       <Card>
         <Image src="https://pbs.twimg.com/profile_banners/1117817856282882048/1599754844/1500x500" />
         <Card.Content>
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,40 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { Button } from "semantic-ui-react";
 import { BrowserRouter, Route } from "react-router-dom";
-import "semantic-ui-css/semantic.min.css";
+import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/header";
 import Work from "./components/work";
-import Highlight from "./components/highlight";
 import TechNode from "./components/techNode";
 import DirectedGraph from "./components/directedGraph";
-import D3test from "./components/d3test"
-import SignUp from "./components/signup"
+import SignUp from "./components/signup";
+import SignIn from "./components/signin";
+import Home from "./components/home";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <div>
-        <a href="/">root</a> | <a href="/work">work</a> |{" "}
-        <a href="/highlight">highlight</a> |<a href="/tech-node">tech-node</a> |
-        <a href="/graph">graph</a> | <a href="/test">test</a> | <a href="/signup">signup</a>
-      </div>
-      <Route exact path="/">
-        <Button>click me</Button>
-      </Route>
-      <Route exact path="/work" component={Work} />
-      <Route exact path="/tech-node" component={TechNode} />
-      <Route exact path="/highlight" component={Highlight} />
-      <Route exact path="/graph" component={DirectedGraph} />
-      <Route exact path="/test" component={D3test} />
-      <Route exact path="/signup" component={SignUp} />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <div
+          style={{ marginTop: "50px", marginLeft: "30px", marginRight: "30px" }}
+        >
+          <Route exact path="/" component={Home} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/tech-node" component={TechNode} />
+          <Route exact path="/graph" component={DirectedGraph} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signin" component={SignIn} />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
