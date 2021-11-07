015266e10fc3fcec9741382ca4a1051097e0fe8c
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-07 13:19:30 +0900

    文章変更

--- front-end/src/components/home.tsx
+++ front-end/src/components/home.tsx
@@ -32,39 +32,46 @@ const Home = () => {
             textShadow: "1px 1px 10px black",
           }}
         >
-          <Header as="h1" style={{ color: "white" }}>
+          <Header as="h1" style={{ color: "white", marginBottom: "2rem" }}>
             Gaming Portfolio
           </Header>
-          <p style={{ color: "white" }}>
+          <p
+            style={{ color: "white", fontSize: "1.2rem", lineHeight: "0.8rem" }}
+          >
             スキルツリーのあるゲームでスキルを解放していくのって楽しいですよね？
           </p>
-          <p style={{ color: "white" }}>
-            自身のスキルをゲームのように<span>可視化</span>
+          <p
+            style={{ color: "white", fontSize: "1.2rem", lineHeight: "0.8rem" }}
+          >
+            自身のスキルをゲームのように
+            <span style={{ fontWeight: "bold", margin: "5px" }}>可視化</span>
             して、楽しく目標設定・スキルアップをしましょう。
           </p>
         </Container>
       </div>
       <Grid
         container
-        columns="3"
-        style={{ marginBottom: "32px", minHeight: "300px" }}
+        relaxed="very"
+        columns="2"
+        style={{ marginBottom: "32px", minHeight: "100px" }}
       >
         <Grid.Column>
           <Header as="h2">理念</Header>
-          <p>
-            技術っていろいろレイヤーがあるよね。人が外から確認できるのは究極的には成果物だけなんだから、そこからどれだけ簡単に情報を引き出せるかを考えよう。
+          <p style={{ lineHeight: "1.8rem" }}>
+            使い慣れた道具ほど扱いやすくなるため、1つの道具に慣れてしまうと他の道具を触るモチベーションが落ちてしまう。
+          </p>{" "}
+          <p style={{ lineHeight: "1.8rem", marginTop: "-6px" }}>
+            そこで様々なスキルをツリー状に並べ、使える道具を可視化することで、他のスキルに手を伸ばすモチベーション作りをしようというコンセプトのもと作成しました。
           </p>
         </Grid.Column>
         <Grid.Column>
           <Header as="h2">特徴</Header>
-          <p>
-            ポートフォリオと使用技術を対応付け、そこからスキルツリーを生成できます。
+          <p style={{ lineHeight: "1.8rem" }}>
+            　ポートフォリオというのは基本的に人に自身の成果物を見せるためのもので、自身の成果物を自分のためにまとめるサービスというのはあまり見かけません。
+          </p>{" "}
+          <p style={{ lineHeight: "1.8rem", marginTop: "-6px" }}>
+            本サービスは、自分のためという点に重きをおいて制作しました。
           </p>
-        </Grid.Column>
-        <Grid.Column>
-          {/* TODO 埋める */}
-          <Header as="h2">ウーッッ</Header>
-          <p>ｷｴｷｴ</p>
         </Grid.Column>
       </Grid>
       <Button
