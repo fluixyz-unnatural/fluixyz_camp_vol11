fdde602bb8a1e5b8ed717ca68e7b7671240bfb15
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-07 09:16:04 +0900

    ポトフぉからレベル算出

--- front-end/src/components/gates/gate.tsx
+++ front-end/src/components/gates/gate.tsx
@@ -3,12 +3,24 @@ import { BrowserRouter, Link, Route } from "react-router-dom";
 import LanguageView from "./languageView";
 import DomainView from "./domainView";
 import GenreView from "./genreView";
+import { createContext, useState, useEffect } from "react";
+import { db } from "../../firebase";
+import { useAuthContext } from "../../context/AuthContext";
+import hash from "object-hash";
 
 interface GateProps {
   label: string;
   path: string;
   color: string;
 }
+
+interface LevelContext {
+  label: string;
+  level: number;
+}
+
+export const WorksContext = createContext<Array<LevelContext>>([]);
+
 const Gate = (props: GateProps) => {
   return (
     <Link to={props.path}>
@@ -18,12 +30,79 @@ const Gate = (props: GateProps) => {
 };
 
 const GateIndex = () => {
+  const [works, setworks] = useState([]);
+  const { user }: any = useAuthContext();
+  const list = [
+    "javascript",
+    "python",
+    "go",
+    "cpp",
+    "ruby",
+    "typescript",
+    "html",
+    "css",
+    "semantic-ui",
+    "vue",
+    "react",
+    "express",
+    "ruby-on-rails",
+    "flask",
+    "django",
+    "three-js",
+    "nuxtjs",
+    "nextjs",
+    "nginx",
+    "apache",
+    "docker",
+    "ec2",
+    "ecs",
+    "lambda",
+    "s3",
+    "amplify",
+    "firebase",
+    "cloud-functions",
+    "compute-engine",
+    "cloud-storage",
+    "container",
+    "nodejs",
+    "web-audio-api",
+    "github-actions",
+    "front-end",
+    "back-end",
+    "infrastructure",
+  ];
+  useEffect(() => {
+    const tmp: Array<LevelContext> = [];
+    list.forEach((e) => {
+      tmp.push({ label: e, level: 0 });
+    });
+    db.collection("works")
+      .where("author", "==", hash.MD5(user.email))
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          console.log(data.techs);
+          data.techs.forEach((tag: string) => {
+            if (list.includes(tag)) {
+              tmp.find((elm) => elm.label == tag).level++;
+            }
+          });
+        });
+      })
+      .catch((err) => {
+        alert(err);
+      });
+    setworks(tmp);
+  }, []);
   return (
-    <>
+    <WorksContext.Provider
+      value={works}
+    >
       <BrowserRouter>
         <div className={"spacer"}></div>
         <div className={"full-container"}>
-          <Route exact path="/gate/">
+          <Route exact path="/gate">
             <Gate path="/gate/domain" label="Domain" color="red" />
             <Gate path="/gate/language" label="Language" color="blue" />
             <Gate path="/gate/genre" label="Genre" color="green" />
@@ -33,7 +112,7 @@ const GateIndex = () => {
           <Route exact path="/gate/genre" component={GenreView} />
         </div>
       </BrowserRouter>
-    </>
+    </WorksContext.Provider>
   );
 };
 
