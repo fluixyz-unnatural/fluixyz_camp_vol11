a060a4fe96546fcc25b164166a96116c7342d831
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-07 13:20:39 +0900

    タグ入力周り

--- front-end/src/components/tagInput.tsx
+++ front-end/src/components/tagInput.tsx
@@ -1,5 +1,102 @@
-const TagInput = () => {
-  return <input placeholder="仮設置"></input>;
+import { useState, useRef } from "react";
+import { Button, Grid, Container } from "semantic-ui-react";
+import Tag from "./tag";
+import dependencies from "../sample/tags3.json";
+
+interface Props {
+  handleChange: Function;
+}
+
+const getParents = (child: string): Array<string> => {
+  const t = dependencies.find((elm) => elm.tag === child);
+  return [t.type, t.domain, t.language, t.genre].filter((e) => e !== undefined);
+};
+
+const TagInput = (props: Props) => {
+  const [tags, settags] = useState<Array<string>>([]);
+  const list = [
+    "javascript",
+    "python",
+    "go",
+    "cpp",
+    "ruby",
+    "typescript",
+    "html",
+    "css",
+    "semantic-ui",
+    "vue",
+    "react",
+    "express",
+    "ruby-on-rails",
+    "flask",
+    "django",
+    "three-js",
+    "nuxtjs",
+    "nextjs",
+    "nginx",
+    "apache",
+    "docker",
+    "ec2",
+    "ecs",
+    "lambda",
+    "s3",
+    "amplify",
+    "firebase",
+    "cloud-functions",
+    "compute-engine",
+    "cloud-storage",
+    "container",
+    "nodejs",
+    "web-audio-api",
+    "github-actions",
+    "front-end",
+    "back-end",
+    "infrastructure",
+    "graphic",
+    "audio",
+    "algorithm",
+  ];
+  const input = useRef(null);
+  const handleClick = (e: Event) => {
+    e.preventDefault();
+    const tmp = [...tags];
+    if (!tmp.includes(input.current.value)) tmp.push(input.current.value);
+    getParents(input.current.value).forEach((e) => {
+      if (!tmp.includes(e)) tmp.push(e);
+    });
+    settags(tmp);
+    console.log(tags);
+    input.current.value = "";
+    // props.handleChange(tags);
+  };
+
+  return (
+    <Container>
+      <div style={{ margin: "10px" }}>
+        {tags.map((e, index) => (
+          <Tag name={e} key={index} />
+        ))}
+      </div>
+      <Grid>
+        <Grid.Column width={10}>
+          <input
+            list="tech"
+            type="text"
+            ref={input}
+            placeholder="仮設置"
+          ></input>
+          <datalist id="tech">
+            {list.map((e) => {
+              return <option value={e} key={e} />;
+            })}
+          </datalist>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <Button onClick={handleClick}>タグ追加</Button>
+        </Grid.Column>
+      </Grid>
+    </Container>
+  );
 };
 
 export default TagInput;
